2c71715968c252ab1ac857f76d8d6c0ff1578a62
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-10-29 12:10:03 +0200

    Import Platform and KeyBoardAvoidingView from react-native

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class Chat extends Component {
