21955809cdf2ebe55a225d7d338c794a99dca414
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-11-12 09:56:51 +0100

    Import InputToolbar

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import NetInfo from '@react-native-community/netinfo';
 const firebase = require('firebase');
